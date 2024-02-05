a0806f98c2a6deb71926a9bca57a3f2df031bbca
GrimdvlLaptop <grimdvl@gmail.com>
2024-02-05 18:18:08 +0200

    create correct animation with scrolling at resume section

--- src/js/modules/navigation.js
+++ src/js/modules/navigation.js
@@ -3,6 +3,8 @@ function navigation(linksSelector, activeClass, sectionsSelector, indicatorSelec
           indicator = document.querySelector(indicatorSelector),
           sections = document.querySelectorAll(sectionsSelector);
 
+    const resume = document.querySelector('.resume__column');
+
     function removeActiveClass() {
         links.forEach((item) => {
             item.classList.remove(activeClass);
@@ -22,13 +24,17 @@ function navigation(linksSelector, activeClass, sectionsSelector, indicatorSelec
                 removeActiveClass();
                 indicator.classList.remove('hide');
                 indicator.classList.add('show');
+                link.classList.add('animated');
+                resume.classList.add('animated');
                 linkParent.classList.add(activeClass);
+                
             } else if (top <= 400 && link) {
                 const linkParent = link.parentNode;
 
                 linkParent.classList.remove(activeClass);
                 indicator.classList.remove('show');
                 indicator.classList.add('hide');
+                // resume.classList.remove('animated');
             }
         });
     }
