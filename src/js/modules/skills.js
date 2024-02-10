101c3cef543db7afe648b1fd3d77e917c9297686
GrimdvlLaptop <grimdvl@gmail.com>
2024-02-10 17:26:27 +0200

    animation fix

--- src/js/modules/skills.js
+++ src/js/modules/skills.js
@@ -2,8 +2,23 @@ function skills(counterSelector, lineSelector) {
     const counters = document.querySelectorAll(counterSelector),
           lines = document.querySelectorAll(lineSelector);
 
-    counters.forEach((item, i) => {
-        lines[i].style.width = item.innerHTML;
+    function numberCounter(counter, target, index) {
+        const value = +counter.innerText.replace(/\D/g, '');
+
+        if (value < target) {
+            counter.innerHTML = `${Math.ceil(value + 1)}<sup>%</sup>`;
+            const progress = (value + 1) / 100 * 100;
+            lines[index].style.width = `${progress}%`;
+            setTimeout(() => {
+                numberCounter(counter, target, index);
+            }, 15);
+        }
+    }
+
+    counters.forEach((counter, i) => {
+        counter.innerHTML = `0<sup>%</sup>`;
+        const target = +counter.getAttribute('value');
+        numberCounter(counter, target, i);
     });
 }
 
