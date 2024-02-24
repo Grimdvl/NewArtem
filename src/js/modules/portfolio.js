de340d17abd3baa7603238193a1a816cff7c84c1
GrimdvlLaptop <grimdvl@gmail.com>
2024-02-24 15:55:14 +0200

    fix animation portfolio mobile

--- src/js/modules/portfolio.js
+++ src/js/modules/portfolio.js
@@ -6,15 +6,44 @@ const portfolioTrigger = (selectorElem, activeClass, selectorParent) => {
         elements.forEach((item) => {
             item.classList.remove(activeClass);
             parent.classList.remove(activeClass);
+            parent.parentNode.classList.remove(activeClass);
+            parent.parentNode.style.marginTop = '0';
+            parent.parentNode.style.marginBottom = '0';
         });
     }
 
     const addActiveClass = () => {
-        elements.forEach((elem) => {
+        elements.forEach((elem, index) => {
             elem.addEventListener('click', (event) => {
                 removeActiveClass();
                 event.target.parentNode.classList.add(activeClass);
                 parent.classList.add(activeClass);
+                
+                if (index === 0) {
+                    parent.parentNode.style.marginTop = '20vh';
+                    parent.parentNode.style.marginBottom = '0';
+                }
+                if (index === 1) {
+                    parent.parentNode.style.marginTop = '15vh';
+                    parent.parentNode.style.marginBottom = '0';
+                }
+                if (index === 2) {
+                    parent.parentNode.style.marginTop = '10vh';
+                    parent.parentNode.style.marginBottom = '0';
+                }
+    
+                if (index === elements.length - 1) {
+                    parent.parentNode.style.marginBottom = '20vh';
+                    parent.parentNode.style.marginTop = '0';
+                }
+                if (index === elements.length - 2) {
+                    parent.parentNode.style.marginBottom = '15vh';
+                    parent.parentNode.style.marginTop = '0';
+                }
+                if (index === elements.length - 3) {
+                    parent.parentNode.style.marginBottom = '10vh';
+                    parent.parentNode.style.marginTop = '0';
+                }
             });
         });
     }
