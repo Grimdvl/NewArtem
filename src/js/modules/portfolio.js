1dd519619d647a4d42f9a6ac9b22bd8aad5774e0
GrimdvlLaptop <grimdvl@gmail.com>
2024-03-07 13:49:04 +0200

    s

--- src/js/modules/portfolio.js
+++ src/js/modules/portfolio.js
@@ -26,10 +26,10 @@ const portfolioTrigger = (selectorElem, activeClass, selectorParent) => {
                         parent.parentNode.style.marginTop = '20vh';
                         parent.parentNode.style.marginBottom = '0';
                     } else if (index >= 0 && index <= Math.floor(elements.length / 2)) {
-                        parent.parentNode.style.marginTop = '11vh';
+                        parent.parentNode.style.marginTop = '10vh';
                         parent.parentNode.style.marginBottom = '0';
                     } else if (index <= elements.length - 1 && index >= Math.floor(elements.length / 2)) {
-                        parent.parentNode.style.marginTop = '15vh';
+                        parent.parentNode.style.marginTop = '5vh';
                         parent.parentNode.style.marginBottom = '10vh';
                     } else if (index === elements.length - 1) {
                         parent.parentNode.style.marginTop = '5vh';
