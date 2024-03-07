d042bae20e174ffd9bef0b8d0f89e12cdb40c4e3
GrimdvlLaptop <grimdvl@gmail.com>
2024-03-07 12:01:55 +0200

    fix port

--- src/js/modules/portfolio.js
+++ src/js/modules/portfolio.js
@@ -28,8 +28,8 @@ const portfolioTrigger = (selectorElem, activeClass, selectorParent) => {
                     } else if (index >= 0 && index <= Math.floor(elements.length / 2)) {
                         parent.parentNode.style.marginTop = '10vh';
                         parent.parentNode.style.marginBottom = '0';
-                    } else if (index >= elements.length - 1) {
-                        parent.parentNode.style.marginTop = '15vh';
+                    } else if (index >= elements.length - 1 && index >= Math.floor(elements.length / 2)) {
+                        parent.parentNode.style.marginTop = '10vh';
                         parent.parentNode.style.marginBottom = '15vh';
                     } else {
                         parent.parentNode.style.marginTop = '0';
