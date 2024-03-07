5dfbf0360a62558a5b47283716bda328692a3606
GrimdvlLaptop <grimdvl@gmail.com>
2024-03-07 12:37:29 +0200

    fix

--- src/js/modules/portfolio.js
+++ src/js/modules/portfolio.js
@@ -29,7 +29,7 @@ const portfolioTrigger = (selectorElem, activeClass, selectorParent) => {
                         parent.parentNode.style.marginTop = '11vh';
                         parent.parentNode.style.marginBottom = '0';
                     } else if (index >= elements.length - 1 && index >= Math.floor(elements.length / 2)) {
-                        parent.parentNode.style.marginTop = '15h';
+                        parent.parentNode.style.marginTop = '15vh';
                         parent.parentNode.style.marginBottom = '15vh';
                     } else {
                         parent.parentNode.style.marginTop = '0';
