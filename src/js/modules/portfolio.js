1dc56d5696a1649f30542546dd7d55c2766dd53b
GrimdvlLaptop <grimdvl@gmail.com>
2024-03-07 11:32:52 +0200

    fixing bugs

--- src/js/modules/portfolio.js
+++ src/js/modules/portfolio.js
@@ -23,13 +23,13 @@ const portfolioTrigger = (selectorElem, activeClass, selectorParent) => {
                 
                 if (mediaQuery) {
                     if (index === 0 ) {
-                        parent.parentNode.style.marginTop = '15vh';
+                        parent.parentNode.style.marginTop = '20vh';
                         parent.parentNode.style.marginBottom = '0';
                     } else if (index >= 0 && index <= Math.floor(elements.length / 2)) {
-                        parent.parentNode.style.marginTop = '10vh';
+                        parent.parentNode.style.marginTop = '15vh';
                         parent.parentNode.style.marginBottom = '0';
                     } else if (index >= elements.length - 1) {
-                        parent.parentNode.style.marginTop = '5vh';
+                        parent.parentNode.style.marginTop = '10vh';
                         parent.parentNode.style.marginBottom = '15vh';
                     } else {
                         parent.parentNode.style.marginTop = '0';
