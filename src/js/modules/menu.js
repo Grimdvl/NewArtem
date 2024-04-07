9d4d39250a8ae0912f872d0f8421099d63561fe0
GrimdvlDesktop <grimdvl@gmail.com>
2024-04-07 23:47:50 +0300

    some changes

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -1,3 +1,6 @@
+import skills from './skills';
+import {loadingSkillsCards} from './cards';
+
 const navigationMenu = (activeClass, menuSelector, navigationSelector, modeSelector, colorSelector) => {
     const menu = document.querySelector(menuSelector),
           nav = document.querySelector(navigationSelector),
@@ -8,11 +11,36 @@ const navigationMenu = (activeClass, menuSelector, navigationSelector, modeSelec
         nav.classList.toggle(activeClass);
     });
 
-
     mode.addEventListener('click', () => {
         mode.classList.toggle(activeClass);
         document.documentElement.classList.toggle(activeClass);
         color.classList.toggle(activeClass);
+
+        const blocks = document.querySelectorAll('.skills__card-front-icon .block');
+        const counters = document.querySelectorAll('.counter');
+        const width = document.querySelectorAll('.skills__ratings-line span');
+        const countersSecond = document.querySelectorAll('.skills__ratings-counter');
+    
+
+        if (document.querySelector('.skills').classList.contains('animated')) {
+            countersSecond.forEach((counterSecond) => {
+                counterSecond.textContent = '';
+            });
+            width.forEach((item) => {
+                item.style.width = 0;
+            });
+            blocks.forEach((block) => {
+                block.remove();
+            });
+            counters.forEach((counter) => {
+                counter.textContent = '';
+            });
+
+            setTimeout(() => {
+                loadingSkillsCards('.skills__card-front-icon', '.counter');
+                skills('.skills__ratings-counter', '.skills__ratings-line span');
+            }, 1000);
+        }
     });
 }
 
