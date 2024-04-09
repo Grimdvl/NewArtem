3073b84989fcd68e46a9ddca626b5eafc9128efd
GrimdvlLaptop <grimdvl@gmail.com>
2024-04-09 17:52:39 +0300

    some changes

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -7,39 +7,45 @@ const navigationMenu = (activeClass, menuSelector, navigationSelector, modeSelec
           mode = document.querySelector(modeSelector),
           color = document.querySelector(colorSelector);
 
+    let hasSkillsLoaded = false;
+
     menu.addEventListener('click', () => {
         nav.classList.toggle(activeClass);
     });
+
+    const clearing = () => {
+        const blocks = document.querySelectorAll('.skills__card-front-icon .block');
+        const counters = document.querySelectorAll('.counter');
+        const width = document.querySelectorAll('.skills__ratings-line span');
+        const countersSecond = document.querySelectorAll('.skills__ratings-counter');
+
+        countersSecond.forEach((counterSecond) => {
+            counterSecond.textContent = '';
+        });
+        width.forEach((item) => {
+            item.style.width = 0;
+        });
+        blocks.forEach((block) => {
+            block.remove();
+        });
+        counters.forEach((counter) => {
+            counter.textContent = '';
+        });
+    }
 
     mode.addEventListener('click', () => {
         mode.classList.toggle(activeClass);
         document.documentElement.classList.toggle(activeClass);
         color.classList.toggle(activeClass);
 
-        const blocks = document.querySelectorAll('.skills__card-front-icon .block');
-        const counters = document.querySelectorAll('.counter');
-        const width = document.querySelectorAll('.skills__ratings-line span');
-        const countersSecond = document.querySelectorAll('.skills__ratings-counter');
-    
-
-        if (document.querySelector('.skills').classList.contains('animated')) {
-            countersSecond.forEach((counterSecond) => {
-                counterSecond.textContent = '';
-            });
-            width.forEach((item) => {
-                item.style.width = 0;
-            });
-            blocks.forEach((block) => {
-                block.remove();
-            });
-            counters.forEach((counter) => {
-                counter.textContent = '';
-            });
-
+        if (document.querySelector('.skills').classList.contains('animated') && !hasSkillsLoaded) {
+            hasSkillsLoaded = true;
+            clearing();
             setTimeout(() => {
                 loadingSkillsCards('.skills__card-front-icon', '.counter');
                 skills('.skills__ratings-counter', '.skills__ratings-line span');
-            }, 1000);
+                hasSkillsLoaded = false;
+            }, 500);
         }
     });
 }
